5dd5259e5fd161b7baf7c4bd12bd5d577de4a020
Frederic Hamel <frederic.hamel123@gmail.com>
2017-05-04 09:54:58 -0400

    Add movestack(+/- 1)

--- dwm.c
+++ dwm.c
@@ -243,6 +243,7 @@ static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
 static void motionnotify(XEvent *e);
 static void movemouse(const Arg *arg);
+static void movestack(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *);
 static void propertynotify(XEvent *e);
@@ -1481,6 +1482,59 @@ movemouse(const Arg *arg) {
 		changemon(m);
 		focus(NULL);
 	}
+}
+void
+movestack(const Arg *arg) {
+  Client *c = NULL, *p = NULL, *pc = NULL, *i;
+  if(arg->i < 0) {
+    // Find client before selmon->sel
+    for(i = selmon->clients; i != selmon->sel; i = i->next) {
+      if (ISVISIBLE(i) && !i->isfloating) {
+        c = i;
+      }
+    }
+    if(!c) {
+      for(; i; i = i->next) {
+        if(ISVISIBLE(i) && !i->isfloating) {
+          c = i;
+        }
+      }
+    }
+  } else {
+    // Find client after selmon->sel
+    for(c = selmon->sel->next; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+    if (!c) {
+      for (c = selmon->clients; c != selmon->sel && (!ISVISIBLE(c) || c->isfloating); c = c->next); 
+    }
+  }
+  
+  for(i = selmon->clients; i && (!p || !pc); i = i->next) {
+    if(i->next == selmon->sel) {
+      p = i;
+    } else if (i->next == c) {
+      pc = i;
+    }
+  }
+
+  if(c && c != selmon->sel) {
+    Client *tmp = selmon->sel->next == c ? selmon->sel: selmon->sel->next;
+    selmon->sel->next = c->next == selmon->sel ? c : c->next;
+    c->next = tmp;
+
+    if(p && p != c) {
+      p->next = c;
+    }
+    if (pc && pc != selmon->sel) {
+      pc->next = selmon->sel;
+    }
+    if (selmon->sel == selmon->clients) {
+      selmon->clients = c;
+    }
+    else if (c == selmon->clients) {
+      selmon->clients = selmon->sel;
+    }
+    arrange(selmon);
+  }
 }
 
 Client *
